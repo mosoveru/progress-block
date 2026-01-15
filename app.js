c6a55e56c287e19be7d31b8f763f20e1bc01f988
Sergey Mosienko <mosoveru@gmail.com>
2026-01-15 10:48:49 +0300

    feat: implement progress bar animation
    Also added new props to ProgressComponent that is meant to set circle colors. Corresponding properties for circle colors was removed from CSS styles.
    Some code improvements.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-import { ProgressComponent } from "./components/progress.js";
+import { ProgressComponent } from './components/progress.js';
 
-const progressRoot = document.getElementById("progressRoot");
-const valueInput = document.getElementById("valueInput");
-const animateSwitch = document.getElementById("animateSwitch");
-const hideSwitch = document.getElementById("hideSwitch");
+const progressRoot = document.getElementById('progressRoot');
+const valueInput = document.getElementById('valueInput');
+const animateSwitch = document.getElementById('animateSwitch');
+const hideSwitch = document.getElementById('hideSwitch');
 
 const progressComponent = new ProgressComponent();
+console.log(progressComponent);
+
+valueInput.addEventListener('change', e => {
+    valueInput.value = progressComponent.setArcValue(e.target.value);
+});
+animateSwitch.addEventListener('change', e => {
+    progressComponent.setAnimate(e.target.checked);
+});
+
 progressComponent.mount(progressRoot);
--- components/progress.js
+++ components/progress.js
@@ -1,5 +1,5 @@
 export class ProgressComponent {
-    static svgNamespace = "http://www.w3.org/2000/svg";
+    static svgNamespace = 'http://www.w3.org/2000/svg';
 
     #svg;
     #track;
@@ -7,22 +7,41 @@ export class ProgressComponent {
     #value = 0;
     #circumference;
 
-    constructor({size = 160, stroke = 12} = {}) {
-        const svg = document.createElementNS(ProgressComponent.svgNamespace, "svg");
-        svg.setAttribute("viewBox", `0 0 ${size} ${size}`);
+    constructor({
+            size = 160,
+            stroke = 12,
+            trackColor = '#EEF3F6',
+            arcColor = '#015DFE',
+            arcTransitionDelay = '0'
+        } = {}
+    ) {
+        const svg = document.createElementNS(ProgressComponent.svgNamespace, 'svg');
+        svg.setAttribute('viewBox', `0 0 ${size} ${size}`);
         svg.setAttribute('role', 'img');
         const cx = size / 2;
         const cy = size / 2;
         const radius = (size - stroke) / 2;
-        const trackCircle = ProgressComponent.#createSVGCircle({cx, cy, radius, stroke});
+        const trackCircle = ProgressComponent.#createSVGCircle({
+            cx,
+            cy,
+            radius,
+            stroke,
+            strokeColor: trackColor
+        });
         trackCircle.setAttribute('class', 'progress__track');
         svg.appendChild(trackCircle);
-        const arc = ProgressComponent.#createSVGCircle({cx, cy, radius, stroke});
+        const arc = ProgressComponent.#createSVGCircle({
+            cx,
+            cy,
+            radius,
+            stroke,
+            strokeColor: arcColor
+        });
         arc.setAttribute('class', 'progress__arc');
+        arc.style.transition = arcTransitionDelay;
         svg.appendChild(arc);
         const circumference = 2 * Math.PI * radius;
         this.#circumference = circumference;
-        arc.setAttribute('transform', `rotate(-90 ${cx} ${cy})`);
         arc.style.strokeDasharray = `${circumference}`;
         arc.style.strokeDashoffset = `${circumference}`;
         this.#arc = arc;
@@ -31,11 +50,14 @@ export class ProgressComponent {
     }
 
     setArcValue(value) {
-        this.#value = value;
         const clampedValue = ProgressComponent.#clampValue(value);
-        const offset = this.#circumference * (1 - clampedValue / 100);
-        this.#arc.style.strokeDashoffset = offset;
-        return this;
+        this.#value = clampedValue;
+        this.#arc.style.strokeDashoffset = this.#circumference * (1 - clampedValue / 100);
+        return clampedValue;
+    }
+
+    setAnimate(isAnimated) {
+        this.#arc.classList.toggle('progress__arc_play', Boolean(isAnimated));
     }
 
     mount(container) {
@@ -46,13 +68,14 @@ export class ProgressComponent {
         return this;
     }
 
-    static #createSVGCircle({ cx, cy, radius, stroke }) {
+    static #createSVGCircle({ cx, cy, radius, stroke, strokeColor }) {
         const circle = document.createElementNS(ProgressComponent.svgNamespace, 'circle');
         circle.setAttribute('cx', String(cx));
         circle.setAttribute('cy', String(cy));
         circle.setAttribute('r', String(radius));
         circle.setAttribute('stroke-width', String(stroke));
         circle.setAttribute('fill', 'none');
+        circle.style.stroke = strokeColor;
         return circle;
     }
 
